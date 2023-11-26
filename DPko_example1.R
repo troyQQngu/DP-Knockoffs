00a13c5144ab90bc3c2fc4c7322ed3ac29170082
Troy Tao <troytao@Troys-MBP.lan>
2023-11-26 15:05:09 -0800

    now using X with normalized rows.

--- DPko_example1.R
+++ DPko_example1.R
@@ -4,39 +4,39 @@ library(distr)
 
 set.seed(1234)
 # model parameters
-n = 500
-p = 500
-k = 30
-amp = 100
+n = 20000
+p = 200
+k = 20
+amp = 5
+B = 1 # normalization of rows of X
 
 # model setup
-mu <- rep(0,p)
 rho = 0.25
 Sigma <- toeplitz(rho^(0:(p-1)))
 X <- matrix(rnorm(n*p),n)%*%chol(Sigma)
-
+X <- X/sqrt(rowSums(X^2))*B
 nonzero <- sample(p,k)
-beta <- amp* (1:p %in% nonzero)/sqrt(n)
-y.sample <- function(X) X %*% beta +1/4*rnorm(n)
+beta <- amp* (1:p %in% nonzero)
+y.sample <- function(X) X %*% beta +rnorm(n)
 y <- y.sample(X)
 
 # privacy parameters
-epsilon = 0.5
-delta = 1e-1
-r = 400 # JLT dimension 
+epsilon = 2.5
+delta = 1e-4
+r = 200 # JLT dimension
+
 
 # lower bounding singular values of [X;y]
-B <- max(sqrt(rowSums(X^2)))
-w_sq <- 8*B^2/epsilon*(sqrt(2*r*log(8/delta))+2*log(8/delta))
+A <- cbind(X,y)
+w_sq <- 4*B^2/epsilon*(sqrt(2*r*log(8/delta))+2*log(8/delta))
 Lap <- DExp(4*B^2/epsilon)
 Z <- r(Lap)(1) 
-A <- cbind(X,y)
 UDV <- svd(A)
 min_sin <- min(UDV$d)
 
 # Johnson-Lindenstrauss transform
 R = 1/sqrt(r)*matrix(rnorm(r*n),r)
-if (TRUE) { #(min_sin^2 > w_sq+Z+4*B^2*log(1/delta)/epsilon) {
+if (min_sin^2 > w_sq+Z+4*B^2*log(1/delta)/epsilon) {
   RX <- R%*%X
   Ry <- R%*%y
   matrix_altered <- FALSE
@@ -48,11 +48,13 @@ if (TRUE) { #(min_sin^2 > w_sq+Z+4*B^2*log(1/delta)/epsilon) {
 
 set.seed(Sys.time())
 tic()
-result = knockoff.filter(RX,Ry)
+result = knockoff.filter(RX,Ry,fdr=0.2)
 toc()
 
 print(result)
 
 fdp = function(selected) sum(beta[selected] == 0)/max(1,length(selected))
 fdp(result$selected)
+power = function(selected) length(selected)/k
+power(result$selected)
 
