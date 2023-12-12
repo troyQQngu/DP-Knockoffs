716862ea2fec5379a9f2bd05305dbed66606e085
Troy Tao <troytao@Troys-MBP.lan>
2023-12-12 15:16:58 -0800

    tested around. should have kept it for reference purposes.

--- DPko_example1.R
+++ DPko_example1.R
@@ -3,11 +3,11 @@ experiment1 <- function(n){
   library(knockoff)
   library(tictoc)
   library(distr)
-  
+  set.seed(1919)
   # model parameters
-  p = 200
-  k = 20
-  amp = 5
+  p = 50
+  k = 15
+  amp = 4.5
   B = 1 # normalization of rows of X
   
   # model sampling
@@ -19,14 +19,14 @@ experiment1 <- function(n){
   beta <- amp* (1:p %in% nonzero)
   
   # privacy parameters
-  epsilon = 0.5
-  delta = 1e-4
+  epsilon = 0.1
+  delta = 1/n
   r = 200 # JLT dimension
-  w_sq <- 4*B^2/epsilon*(sqrt(2*r*log(8/delta))+2*log(8/delta))
+  w_sq <- 8*B^2/epsilon*(sqrt(2*r*log(8/delta))+2*log(8/delta))
   Lap <- DExp(4*B^2/epsilon)
   Z <- r(Lap)(1)
   
-  num_iter = 100 # number of iterations to approximate FDR and power
+  num_iter = 20 # number of iterations to approximate FDR and power
   
   Xbeta = X %*% beta
   fdps = rep(0,num_iter)
@@ -42,12 +42,14 @@ experiment1 <- function(n){
     min_sin <- min(UDV$d)
     
     # Johnson-Lindenstrauss transform
-    R = 1/sqrt(r)*matrix(rnorm(r*n),r)
+    
     if (min_sin^2 > w_sq+Z+4*B^2*log(1/delta)/epsilon) {
+      R = 1/sqrt(r)*matrix(rnorm(r*n),r)
       RX <- R%*%X
       Ry <- R%*%y
       matrix_altered <- FALSE
     } else {
+      R = 1/sqrt(r)*matrix(rnorm(r*(n+p+1)),r)
       A_alt <- rbind(A,diag(rep(sqrt(w_sq),p+1)))
       RA <- R%*%A_alt
       RX <- RA[,1:p]
@@ -59,7 +61,13 @@ experiment1 <- function(n){
     fdps[i] <- fdp(result$selected)
     powers[i] <- power(result$selected)
     cat(rep("-",50),"\n")
-    cat(i,"/",num_iter, " iteration for n =",n,"\nDP Knockoffs select:", result$selected, "\nTrue model:", sort(nonzero),"\nFDP =",fdps[i],"Power =", powers[i],"\n")
+    cat(i,"/",num_iter, " iteration for n =",n,
+        "\nw^2 =",w_sq,"square of the minimum singular value =", min_sin^2,
+        "matrix altered?:", matrix_altered,
+        "\nDP Knockoffs select:", result$selected, 
+        "\nTrue model:", sort(nonzero),
+        "\nFDP =",fdps[i],
+        "Power =", powers[i],"\n")
   }
   
   cat(rep("=",50),"\n")
@@ -71,8 +79,8 @@ experiment1 <- function(n){
 fdrs <- rep(0,10)
 powers <- rep(0,10)
 
-for (i in 1:10) {
-  n = 50000+10000*i
+for (i in 0:10) {
+  n = 600000+10000*i
   result <- experiment1(n)
   fdrs[i] = result$fdr
   powers[i] = result$power
